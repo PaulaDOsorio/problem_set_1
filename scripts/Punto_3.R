1161263633a327c304acd4b995742e0a8bcbf68b
EriMilenA <e.maciass@uniandes.edu.co>
2024-03-03 13:27:04 -0500

    Punto_3(1)

--- scripts/Punto_3.R
+++ scripts/Punto_3.R
@@ -16,32 +16,20 @@ summary(regresion1)
 str(regresion1)
 regresion1$coefficients
 
+library(readxl)
+datos_t <- read_excel("C:/Users/USUARIO/Documents/GitHub/problem_set_1/views/db_organizada.xlsx")
+
 #Usar booBootstrap para construir los intervalos de confianza
 
 require(pacman)
 library(bootstrap)
 p_load("boot")
 
-# Definir el objeto index
-index <- datos$indice
-
-# Ajustar el modelo de regresión lineal utilizando la variable index para subconjuntar los datos
-modelo <- lm(log_salario_m ~ edad + edad_2, data = datos, subset = index)
-
-# Obtener el segundo coeficiente del modelo
-coeficiente <- coef(modelo)[2]
-
-
-eta_fn<-function(data,index){
-  
-  coef(lm(log_salario_m ~ edad+edad_2, data = datos, subset = index))[2] #returns the second coefficient of the linear regression
-}
-
 u_err_est<-function(data,index){
   coef(lm(log_salario_m~edad + edad_2, data = datos, subset = index)) 
 }
 
-u_err_est(datos_t,1:nrow(datos_t))
+u_err_est(datos,1:nrow(datos))
 
 bootstrap <- boot(datos_t, u_err_est, R = 1000)
 coef_bootstrap <- bootstrap$t0
